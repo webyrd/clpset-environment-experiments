8e9bc97d22415c7146a28e04209bb0932304a5b2
William Byrd <webyrd@gmail.com>
2024-05-13 22:14:18 -0400

    added tests

--- relational-interp/wills-clpset-tests.scm
+++ relational-interp/wills-clpset-tests.scm
@@ -40,3 +40,52 @@
       (== (list x y) q)
       (== (set ∅ x 2) (set ∅ y 1))))
   '((1 2)))
+
+(test-check "d1"
+  (run-unique* (q)
+    (=/= ∅ ∅))
+  '())
+
+(test-check "d2"
+  (run-unique* (q)
+    (=/= (set ∅ 1 2 1) (set ∅ 2 2 1)))
+  '())
+
+(test-check "d3a"
+  (run-unique* (q)
+    (fresh (s1 s2)
+      (== (list s1 s2) q)
+      (seto s1)
+      (seto s2)
+      (== s1 s2)
+      (=/= s1 s2)))
+  '())
+
+(test-check "d3b"
+  (run-unique* (q)
+    (fresh (s1 s2)
+      (== (list s1 s2) q)
+      (seto s1)
+      (seto s2)
+      (=/= s1 s2)
+      (== s1 s2)))
+  '())
+
+(test-check "d3c"
+  (run-unique* (q)
+    (fresh (s1 s2)
+      (== (list s1 s2) q)
+      (=/= s1 s2)
+      (seto s1)
+      (seto s2)
+      (== s1 s2)))
+  '())
+
+(test-check "d4"
+  (run-unique* (q)
+    (fresh (x y)
+      (== (list x y) q)
+      (=/= (set ∅ x 2) (set ∅ 1 2))))
+  '(((_.0 _.1) : (=/= (_.0 1) (_.0 2)))
+    ;; reifer should discard the first answer, which is subsumed by the second
+    ((_.0 _.1) : (=/= (_.0 1)))))
